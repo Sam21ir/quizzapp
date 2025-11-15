c94e029b1840d2675184f479cd729048f80bec8a
SAMIR EL ALAMI <elalamisamirr@gmail.com>
2025-11-15 13:03:11 +0100

    fix case sensitive file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer';
+import Navbar from './components/navbar.jsx';
+import Footer from './components/footer.jsx';
 import Home from './pages/home.jsx';
 import Quiz from './pages/quiz.jsx';
 import Result from './pages/result.jsx';
